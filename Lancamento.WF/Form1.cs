620b55baaa3311e4e8d441bcd194648c0d598474
TI-Desenvolvimento DDA (Felipe) <fozako@metagal.com.br>
2023-11-02 09:36:46 -0300

    Create Windows Forms and Class

--- Lancamento.WF/Form1.cs
+++ Lancamento.WF/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Lancamento.WF
 {
@@ -15,6 +16,34 @@ namespace Lancamento.WF
         public Form1()
         {
             InitializeComponent();
+
+            // Cria um novo gráfico
+            Chart chart = new Chart();
+            chart.Size = new System.Drawing.Size(400, 300);
+
+            // Cria uma nova área de gráfico
+            ChartArea chartArea1 = new ChartArea();
+            chartArea1.AxisX.Minimum = -2; // Define o valor mínimo do eixo X
+            chartArea1.AxisX.Maximum = 2;  // Define o valor máximo do eixo X
+            chartArea1.AxisY.Minimum = -2;  // Define o valor mínimo do eixo Y
+            chartArea1.AxisY.Maximum = 2;   // Define o valor máximo do eixo Y
+
+            // Adiciona a área de gráfico ao gráfico
+            chart.ChartAreas.Add(chartArea1);
+
+            // Adiciona o gráfico ao controle de formulário
+            this.Controls.Add(chart);
+
+            // Adiciona uma série ao gráfico
+            Series series = new Series();
+            chart.Series.Add(series);
+
+            // Adiciona alguns pontos ao gráfico (por exemplo, uma função seno)
+            for (double x = -2; x <= 2; x += 0.1)
+            {
+                double y = Math.Sin(x); // Calcula o valor y usando a função seno
+                series.Points.AddXY(x, y); // Adiciona o ponto ao gráfico
+            }
         }
     }
 }
