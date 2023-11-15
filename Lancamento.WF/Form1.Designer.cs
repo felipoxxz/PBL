c114922ccc1489ffa44255afa3699b49d49e101b
TI-Desenvolvimento DDA (Felipe) <fozako@metagal.com.br>
2023-11-15 09:55:04 -0300

    Alterações 15-11

--- Lancamento.WF/Form1.Designer.cs
+++ Lancamento.WF/Form1.Designer.cs
@@ -28,49 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.txtVelocidade = new System.Windows.Forms.TextBox();
             this.txtAngulo = new System.Windows.Forms.TextBox();
             this.btnCalcular = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // txtVelocidade
             // 
-            this.txtVelocidade.Location = new System.Drawing.Point(1159, 132);
-            this.txtVelocidade.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtVelocidade.Location = new System.Drawing.Point(1738, 203);
             this.txtVelocidade.Name = "txtVelocidade";
-            this.txtVelocidade.Size = new System.Drawing.Size(68, 20);
+            this.txtVelocidade.Size = new System.Drawing.Size(100, 26);
             this.txtVelocidade.TabIndex = 0;
             // 
             // txtAngulo
             // 
-            this.txtAngulo.Location = new System.Drawing.Point(1159, 74);
-            this.txtAngulo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAngulo.Location = new System.Drawing.Point(1738, 114);
             this.txtAngulo.Name = "txtAngulo";
-            this.txtAngulo.Size = new System.Drawing.Size(68, 20);
+            this.txtAngulo.Size = new System.Drawing.Size(100, 26);
             this.txtAngulo.TabIndex = 1;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(1177, 210);
-            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalcular.Location = new System.Drawing.Point(1766, 323);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(50, 27);
+            this.btnCalcular.Size = new System.Drawing.Size(75, 42);
             this.btnCalcular.TabIndex = 2;
             this.btnCalcular.Text = "button1";
             this.btnCalcular.UseVisualStyleBackColor = true;
-            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(323, 99);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(664, 439);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(854, 274);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(90, 40);
+            this.btnIniciar.TabIndex = 4;
+            this.btnIniciar.Text = "Lançar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(1924, 1050);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.btnCalcular);
             this.Controls.Add(this.txtAngulo);
             this.Controls.Add(this.txtVelocidade);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +111,8 @@
         private System.Windows.Forms.TextBox txtVelocidade;
         private System.Windows.Forms.TextBox txtAngulo;
         private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnIniciar;
     }
 }
 
--- Lancamento.WF/Form1.cs
+++ Lancamento.WF/Form1.cs
@@ -13,50 +13,63 @@ namespace Lancamento.WF
 {
     public partial class Form1 : Form
     {
+        private const double Gravidade = 9.8; // Aceleração devido à gravidade em m/s^2
+        private double tempo = 0.1; // Incremento de tempo para a animação
+        private double velocidadeProjetil = 30.0; // Velocidade inicial em m/s
+        private double anguloLancamento = 45.0; // Ângulo de lançamento em graus
+        private double posX, posY; // Posição do objeto no gráfico
+        private double alvoX = 50.0; // Posição inicial do alvo em X
+        private double alvoY = 100.0; // Posição inicial do alvo em Y
+        private double velocidadeAlvo = -2.0; // Velocidade constante do alvo
+
         public Form1()
         {
             InitializeComponent();
-
-            // Cria um novo gráfico
-            Chart chart = new Chart();
-            chart.Size = new System.Drawing.Size(1920, 1080);
-
-            // Cria uma nova área de gráfico
-            ChartArea chartArea1 = new ChartArea();
-            chartArea1.AxisX.Minimum = -2; // Define o valor mínimo do eixo X
-            chartArea1.AxisX.Maximum = 2;  // Define o valor máximo do eixo X
-            chartArea1.AxisY.Minimum = -2;  // Define o valor mínimo do eixo Y
-            chartArea1.AxisY.Maximum = 2;   // Define o valor máximo do eixo Y
-
-            // Adiciona a área de gráfico ao gráfico
-            chart.ChartAreas.Add(chartArea1);
-
-            // Adiciona o gráfico ao controle de formulário
-            this.Controls.Add(chart);
-
-            // Adiciona uma série ao gráfico
-            Series series = new Series();
-            chart.Series.Add(series);
-
-            // Adiciona alguns pontos ao gráfico (por exemplo, uma função seno)
-            for (double x = -2; x <= 2; x += 0.1)
-            {
-                double y = Math.Sin(x); // Calcula o valor y usando a função seno
-                series.Points.AddXY(x, y); // Adiciona o ponto ao gráfico
-            }
+            chart1.Series.Add("Projectile");
+            chart1.Series["Projectile"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            chart1.Series.Add("Target");
+            chart1.Series["Target"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
         }
 
-        private void btnCalcular_Click(object sender, EventArgs e)
+        private void btnIniciar_Click(object sender, EventArgs e)
         {
-            // Obtenha a velocidade e o ângulo do usuário dos TextBox
-            if (double.TryParse(txtVelocidade.Text, out double velocidade) &&
-                double.TryParse(txtAngulo.Text, out double angulo))
+            Timer timer = new Timer();
+            timer.Interval = 100; // Intervalo em milissegundos
+            timer.Tick += new EventHandler(AtualizarPosicao);
+            timer.Start();
+        }
+
+        private void AtualizarPosicao(object sender, EventArgs e)
+        {
+            double radianos = anguloLancamento * (Math.PI / 180.0);
+
+            // Equações de movimento oblíquo
+            double x = velocidadeProjetil * Math.Cos(radianos) * tempo;
+            double y = (velocidadeProjetil * Math.Sin(radianos) * tempo) - (0.5 * Gravidade * Math.Pow(tempo, 2));
+
+            // Atualiza a posição no gráfico
+            chart1.Series["Projectile"].Points.AddXY(x, y);
+
+            // Limpa os pontos antigos da série "Target"
+            chart1.Series["Target"].Points.Clear();
+
+            // Atualiza a posição do alvo no gráfico
+            chart1.Series["Target"].Points.AddXY(alvoX, alvoY);
+
+            tempo += 0.1;
+
+            alvoY += velocidadeAlvo;
+
+            // Se atingir o solo, pare a animação
+            if (y < 0)
             {
-                MessageBox.Show(txtVelocidade.Text);
+                ((Timer)sender).Stop();
             }
-            else
+
+            if (Math.Abs(x) < 1 && Math.Abs(y - alvoY) < 1)
             {
-                MessageBox.Show("Por favor, insira valores válidos para a velocidade e o ângulo.");
+                ((Timer)sender).Stop();
+                MessageBox.Show("Projétil atingiu o alvo!");
             }
         }
     }
